2d5f281310b27c102a8c2810953416cc2ec258b4
Aziz Kale <64408361+azizkale@users.noreply.github.com>
2023-02-05 13:56:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
 
-    TOKEN_DEEPGRAM: "79c607c5513cd211a99ca6d04fc927b52480f84f",
-    ChatGPT_SERVER_URL: "https://sore-gilet-bass.cyclic.app/"
+    TOKEN_DEEPGRAM: "???",
+    ChatGPT_SERVER_URL: "???"
 
 
 
