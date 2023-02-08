d4a93e71294ae9eb4dd409e82bc2287677f79ec5
Aziz Kale <64408361+azizkale@users.noreply.github.com>
2023-02-08 08:03:05 +0100

    Update requestToChatGPTServer.js

--- Scripts/requestToChatGPTServer.js
+++ Scripts/requestToChatGPTServer.js
@@ -1,5 +1,5 @@
 const sendRequestToChatGPT = async(transcript) => {
-    await fetch(ChatGPT_SERVER_URL, {
+    await fetch(CHATGPT_SERVER_URL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
